a1161b307a55b8e2a4f1317b8111b11522c296d5
dapu81000 <50208217+dapu81000@users.noreply.github.com>
2019-05-02 22:51:21 +0800

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	ios::sync_with_stdio();
+	cin.tie(0);
+	int t,x;cin>>t;
+	while(t--){
+		cin>>x;
+		if(x<=15) puts("KFCCA");
+		else puts("PCCA");
+	}
+}
